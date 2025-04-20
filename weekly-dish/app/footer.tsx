98d1ab5a5c0d51ffa83f4677aec49e223c7c3aed
TonChan0828 <showxillia1028@yahoo.co.jp>
2025-04-20 16:29:05 +0900

    add:ページレイアウトのモックを作成

---
+++ weekly-dish/app/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+      <div className="bg-green-500 h-12">
+        <p>Footer</p>
+      </div>
+    </>
+  );
+}
---
+++ weekly-dish/app/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <div className="bg-blue-600 h-12">
+        <p>Header</p>
+      </div>
+    </>
+  );
+}
