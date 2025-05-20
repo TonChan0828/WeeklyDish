e89d891e901e769508aa752347acdc033ced660f
TonChan0828 <showxillia1028@yahoo.co.jp>
2025-05-20 22:36:33 +0900

    add:ヘッダーに各ページのリンクを追加

--- weekly-dish/app/header.tsx
+++ weekly-dish/app/header.tsx
@@ -1,10 +1,21 @@
+"use client";
 import React from "react";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <>
-      <div className="bg-blue-600 h-12">
+      <div className="bg-blue-600 h-12 flex-auto">
         <p>Header</p>
+        <Link href="/profile" className="text-white ml-auto">
+          Profile
+        </Link>
+        <Link href="/calendar" className="text-white ml-4">
+          Calendar
+        </Link>
+        <Link href="/shopping-list" className="text-white ml-4">
+          Shopping List
+        </Link>
       </div>
     </>
   );
